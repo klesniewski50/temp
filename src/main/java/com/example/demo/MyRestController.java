4275a977d35232f5c2fb7b0f0c6472d788dbdd94
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-10-07 14:17:22 +0200

    pierwsza funkcja

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -1,4 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MyRestController {
+
+    @GetMapping("/")
+    String firstFunction(){
+        return "";
+    }
+
 }
