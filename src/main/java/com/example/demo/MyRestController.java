f33dde768f293a016ae2f85d43f12ed73bdc9332
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-10-07 14:43:58 +0200

    próba

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class MyRestController {
 
-    @GetMapping(value = "/{text}")
-    public String findById(@PathVariable("text") String text) {
+    @GetMapping(value = "/rest")
+    public String reverse(@RequestParam(value = "text", defaultValue = "Karol") String text, Model model) {
         StringBuilder builder = new StringBuilder();
         builder.append(text);
         builder.reverse();
         System.out.println(builder.toString());
+        model.addAttribute("reverse", builder.toString());
         return "index";
     }
 
