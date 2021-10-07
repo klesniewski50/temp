f1f3241ff6b59194c7c84accf4c204680bdb1161
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-10-07 14:51:03 +0200

    dodałem odpowiednią adnotację o której całkowicie zapomniałem

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -1,21 +1,19 @@
 package com.example.demo;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class MyRestController {
 
-    @GetMapping(value = "/rest")
+    @RequestMapping(value = "/rest")
     public String reverse(@RequestParam(value = "text", defaultValue = "Karol") String text, Model model) {
         StringBuilder builder = new StringBuilder();
         builder.append(text);
         builder.reverse();
         System.out.println(builder.toString());
         model.addAttribute("reverse", builder.toString());
-        return "index";
+        return "";
     }
 
 }
