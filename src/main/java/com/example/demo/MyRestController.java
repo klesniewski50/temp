efcfe4534a546d867d6c4027ed59eb0db4b63b77
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-10-07 14:55:15 +0200

    finalna wersja

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -1,19 +1,17 @@
 package com.example.demo;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyRestController {
 
-    @RequestMapping(value = "/rest")
-    public String reverse(@RequestParam(value = "text", defaultValue = "Karol") String text, Model model) {
+    @RequestMapping(value = "/rest") //example: http://localhost:8080/rest?text=Karol
+    public String reverse(@RequestParam(value = "text", defaultValue = "Karol") String text) {
         StringBuilder builder = new StringBuilder();
         builder.append(text);
         builder.reverse();
         System.out.println(builder.toString());
-        model.addAttribute("reverse", builder.toString());
-        return "";
+        return builder.toString();
     }
 
 }
